1afa424b5d211975ceeb167c08a9086bc369a328
Pranta Das <pdas@appdynamics.com>
2013-07-11 16:16:24 -0700

    Deleted build directories

--- build/temp/custom/actions/notify-splunk-of-event/notify-splunk-of-event.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rem ------------------------------------------------
-rem Batch file to send event notifications to Splunk
-rem ------------------------------------------------
-..\..\..\jdk/bin/java -Dlog4j.configuration=file:..\..\conf/log4j.xml -jar ..\..\lib\splunkClient.jar Event %*
--- build/temp/custom/actions/notify-splunk-of-policy-violation/notify-splunk-of-policy-violation.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rem ------------------------------------------------------------
-rem Batch file to send policy violation  notifications to Splunk
-rem ------------------------------------------------------------
-..\..\..\jdk\bin\java -Dlog4j.configuration=file:..\..\conf/log4j.xml -jar ..\..\lib\splunkClient.jar PolicyViolation %*
